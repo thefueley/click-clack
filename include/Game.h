4df563e769604c342cb12804959e49c541edd446
TheFueley <5083152+thefueley@users.noreply.github.com>
2017-11-29 10:53:13 -0500

    Add FSM framework
    Begin conversion of "switch" tree to a finite state machine.

--- include/Game.h
+++ include/Game.h
@@ -2,7 +2,44 @@
 #define GAME_H
 
 #include <vector>
+#include <boost/statechart/state_machine.hpp>
+#include <boost/statechart/simple_state.hpp>
+#include <boost/statechart/transition.hpp>
 #include "Player.h"
+
+// Events
+struct EvMoneyUp : boost::statechart::event< EvMoneyUp > {};
+struct EvRoll2 : boost::statechart::event< EvRoll2 > {};
+
+// States
+struct ST_Ante;
+struct ST_Comeout;
+struct ST_Point;
+
+// State Machine
+struct SM_Craps : boost::statechart::state_machine< SM_Craps, ST_Ante >
+{
+    //
+};
+
+// States
+struct ST_Ante : boost::statechart::simple_state< ST_Ante, SM_Craps >
+{
+    ST_Ante() { std::cout << "Entered ST_Ante state\n"; }
+    ~ST_Ante() { std::cout << "Exited ST_Ante state\n"; }
+};
+
+struct ST_Comeout : boost::statechart::simple_state< ST_Comeout, SM_Craps >
+{
+    ST_Comeout() { std::cout << "Entered ST_Comeout state\n"; }
+    ~ST_Comeout() { std::cout << "Exited ST_Comeout state\n"; }
+};
+
+struct ST_Point : boost::statechart::simple_state< ST_Point, SM_Craps >
+{
+    ST_Point() { std::cout << "Entered ST_Point state\n"; }
+    ~ST_Point() { std::cout << "Exited ST_Point state\n"; }
+};
 
 class Game
 {
